f66cca8a79f8b52ad9b520916378e50832010a3b
21011415PhongNC <21011415@st.phenikaa-uni.edu.vn>
2024-05-15 20:52:33 +0700

    laravel

---
+++ helloworld/resources/views/helloworld.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/helloworld.css">
+</head>
+<body>
+     <div class="content">
+        <h1 class="title">
+            Hello world!!
+            <div class="aurora">
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+                <div class="item"></div>
+            </div>
+        </h1>
+     </div>
+</body>
+</html>
